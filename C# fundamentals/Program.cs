cba1a0fb9cc9017a70516771a19e6cfa0269bdef
Naposetivi <110626741+Naposetivi@users.noreply.github.com>
2023-07-09 12:41:26 +0300

    Add project files.

---
+++ C# fundamentals/Program.cs
@@ -0,0 +1,15 @@
+﻿using BethanysPieShopHRM;
+
+Employee bethany = new("Bethany", "Smith", "bethany@snowball.be", new DateTime (1979, 1, 16), 25, EmployeeType.Manager);
+
+bethany.DisplayEmployeeDetails();
+bethany.PerformWork();
+bethany.PerformWork(5);
+bethany.PerformWork();
+bethany.ReceiveWage();
+
+
+WorkTask task;
+task.description = "Bake a delicious pies";
+task.hours = 3;
+task.PerformWorkTask();
